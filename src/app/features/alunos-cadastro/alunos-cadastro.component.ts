a24c624cbe08644d9d5c0adcb5e02ed0a7c7141e
Eder Sparenberger <edsparen@gmail.com>
2024-08-10 14:24:51 -0300

    feat(aluno-cadastro): Updates component with registration form, styling, and localstorage saving

--- src/app/features/alunos-cadastro/alunos-cadastro.component.ts
+++ src/app/features/alunos-cadastro/alunos-cadastro.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alunos-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './alunos-cadastro.component.html',
-  styleUrl: './alunos-cadastro.component.scss'
+  styleUrl: './alunos-cadastro.component.scss',
 })
 export class AlunosCadastroComponent {
+  registrationForm: FormGroup;
+  courses: string[] = ['Matemática', 'Biologia', 'História', 'Informática'];
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registrationForm = this.fb.group({
+      fullName: ['', Validators.required],
+      cpf: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+      course: ['', Validators.required],
+    });
+  }
+
+  get fullName() {
+    return this.registrationForm.get('fullName')!;
+  }
+
+  get cpf() {
+    return this.registrationForm.get('cpf')!;
+  }
+
+  get email() {
+    return this.registrationForm.get('email')!;
+  }
+
+  get phone() {
+    return this.registrationForm.get('phone')!;
+  }
+
+  get course() {
+    return this.registrationForm.get('course')!;
+  }
+
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      console.log('Dados do usuário:', this.registrationForm.value);
+      localStorage.setItem('studentData', JSON.stringify(this.registrationForm.value));
+
+      alert('Usuário salvo com sucesso');
+
+      // Redireciona para a tela de listagem de usuários
+      this.router.navigate(['/alunos']);
+    }
+  }
 }
