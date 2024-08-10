bbca4abc95b133160adb67bd1751e892b1a023e9
Eder Sparenberger <edsparen@gmail.com>
2024-08-10 14:04:01 -0300

    feat(login): Updates login component with login form,validation and styling

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  forgotPassword() {
+    alert('Processo de recuperação de senha enviado para o e-mail cadastrado');
+  }
 }
