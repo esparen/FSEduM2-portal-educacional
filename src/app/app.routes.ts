d02da93e06fcea27e748c3dabe9d3cc724a00dff
Ederson Sparenberger <72260390+esparen@users.noreply.github.com>
2024-08-10 13:16:25 -0300

    Feat/initial components and routes (#1)
    * feat(header): Adds header shared component
    
    * feat(sidebar): Adds sidebar shared component
    
    * feat(alunos): Add alunos feature component
    
    * feat(aluno-cadastro): Add aluno-cadastro feature component
    
    * feat: Add disciplinas feature component
    
    * feat(home): Add home feature component
    
    * feat(login): Add login feature component
    
    * feat(routes): Add routes for login, home, alunos, alunos-cadastro, and disciplinas components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { AlunosComponent } from './features/alunos/alunos.component';
+import { AlunosCadastroComponent } from './features/alunos-cadastro/alunos-cadastro.component';
+import { DisciplinasComponent } from './features/disciplinas/disciplinas.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'alunos', component: AlunosComponent},
+  {path: 'alunos/cadastro', component: AlunosCadastroComponent},
+  {path: 'disciplinas', component: DisciplinasComponent},
+];
---
+++ src/app/features/alunos-cadastro/alunos-cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alunos-cadastro',
+  standalone: true,
+  imports: [],
+  templateUrl: './alunos-cadastro.component.html',
+  styleUrl: './alunos-cadastro.component.scss'
+})
+export class AlunosCadastroComponent {
+
+}
