c40112114b6f6ef0c15cdd091c624cd019f34182
Eder Sparenberger <edsparen@gmail.com>
2024-08-12 11:25:02 -0300

    feat: Update auth service and login component to use json server for credentials

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 interface User {
   name: string;
@@ -13,22 +16,23 @@ interface User {
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private router: Router) {}
+  private usersApiUrl = 'http://localhost:3000/users';
+  
+  constructor(private router: Router, private http: HttpClient) {}
 
-  private user: User | null = {
-    name: 'João da Silva',
-    password: 'admin',
-    username: 'admin@admin.com',
-    avatar: 'assets/joao-avatar.png',
-    isAdmin: true,
-  };
-
-  login(username: string, password: string): boolean {  
-    if (username === this.user?.username && password === this.user?.password) { 
-      sessionStorage.setItem('user', JSON.stringify(this.user));
-      return true;
-    } 
-    return false;
+  login(email: string, password: string): Observable<boolean> {
+    return this.http
+      .get<any[]>(`${this.usersApiUrl}?email=${email}&password=${password}`)
+      .pipe(
+        map((users) => {
+          if (users.length > 0) {
+            const user = users[0];
+            sessionStorage.setItem('user', JSON.stringify(user));
+            return true;
+          }
+          return false;
+        })
+      );
   }
 
   getLoggedInUser(): User | null {
